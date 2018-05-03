4d5a3e2e6bbdffecb08fa5d306f9d6dc2cf89a0d
SighvaturSveinsson <sighvatursv373@nemi.tskoli.is>
2018-05-03 09:42:14 +0000

    Add files via upload

--- lokaverk/lokaverk.py
+++ lokaverk/lokaverk.py
@@ -1,7 +1,17 @@
 #Sighvatur Sveinsson
-#Nota plugins: Flask, Alchemy?, mysqlLite?
 
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 from flask import Flask, render_template, redirect
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
 
 app = Flask(__name__)
 
@@ -10,13 +20,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login',  methods=['GET', 'POST'])
 def login():
+    if  u.set_password == u.check_password:
+        return redirect('/')
     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/myndir')
 def myndir():
-    return render_template('myndir.html')
+    if logged in:
+        v = true
+        #ef v er true sýnir template option um að breyta
+    return render_template('myndir.html', login=v)
 
 @app.route('/contact')
 def contact():
